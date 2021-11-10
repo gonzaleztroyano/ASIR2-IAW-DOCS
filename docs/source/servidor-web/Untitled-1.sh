3ca38b97a4e13fc4001f735a7ea91063b821a7e1
Pablo González <pablogontroya@gmail.com>
2021-11-10 14:07:17 +0100

    21110

--- docs/source/servidor-web/Untitled-1.sh
+++ docs/source/servidor-web/Untitled-1.sh
@@ -4,11 +4,12 @@ echo "Creando usuario" + $1
 
 read -p "Pulsa cualquier tecla para continuar"
 
-
     mkdir /var/www/$1/
     mkdir /var/www/$1/web
     mkdir /var/www/$1/ficheros
     mkdir /var/www/$1/ficheros/logs
+
+# TODO: que no pida introducir la contraseña, confirmación ni datos adicionales
     adduser --no-create-home --home /var/www/$1 --shell /bin/false $1
     chmod 755 /var/www/$1/
     chown -R $1:$1 /var/www/$1/
@@ -22,30 +23,43 @@ printf "
     ServerName $1.iaw.com
     ServerAdmin webmaster@localhost
     DocumentRoot /var/www/$1/web
-    ErrorLog /var/log/apache2/$1-error.log
-    CustomLog $/var/log/apache2/$1-access.log combined
+    ErrorLog /var/www/$1/ficheros/logs/$1-error.log
+    CustomLog /var/www/$1/ficheros/logs/$1-access.log combined
     AssignUserID $1 $1
-</VirtualHost>" > /etc/apache2/sites-available/$1.conf
+</VirtualHost>
+" > /etc/apache2/sites-available/$1.conf
 
 echo "CONFIGURACIÓN DEL SITIO AÑADIDO"
 
 a2ensite $1.conf
 systemctl reload apache2
 
-printf " 
 
-# En /etc/ssh/sshd_config añadimos:
-#   Include /etc/ssh/sshd.d/*.conf
+printf "ChrootDirectory /var/www/$1" >> /etc/ssh/sshd_config.d/$1.conf
 
 
-Match User $1
-        ChrootDirectory %h
+
+#El script debe:
+# Listar usuarios
+# Crear usuarios
+# Borrar usuarios
+# Modificar usuarios
+
+#La web debe:
+# /web
+# /blog
+# /blog
+# /files
+
+#<usuario>.iaw.com
+#blog.<usuario>.iaw.com
+
+
+En sshd_config
+    Match User *
+    Include /etc/ssh/sshd_config.d/*
+
+En sshd_config.d/pepito.conf
+            ChrootDirectory %h
         ForceCommand internal-sftp -u 0027
         PasswordAuthentication yes
-        " >> /etc/ssh/sshd.d/$1.conf
-
-Match User pepito
-        ChrootDirectory %h
-        ForceCommand internal-sftp -u 0027
-        PasswordAuthentication yes
-
