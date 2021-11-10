1dddd4e2a89fefb125f9e1992c6f323aa387aa40
Pablo González <pablogontroya@gmail.com>
2021-11-11 00:14:35 +0100

    Script

--- docs/source/servidor-web/Untitled-1.sh
+++ docs/source/servidor-web/Untitled-1.sh
@@ -1,5 +1,75 @@
 #!/bin/bash
 
+#Función LISTAR
+function listar(){
+
+}
+
+#FUNCION 
+
+#Función CREAR
+function crear(){
+    echo "Creando usuario" + $1
+    read -p "Pulsa cualquier tecla para continuar"
+    mkdir /var/www/$1/
+    mkdir /var/www/$1/web
+    mkdir /var/www/$1/ficheros
+    mkdir /var/www/$1/ficheros/logs
+
+    password_generada=$(openssl rand -base64 12)
+
+    # TODO: #2 que no pida introducir la contraseña, confirmación ni datos adicionales
+    useradd -M --home /var/www/$1 --shell /bin/false -p (perl -e'print crypt($password_generada, "aa")') $1
+    chmod 755 /var/www/$1/
+    chown -R $1:$1 /var/www/$1/
+    chown root:root /var/www/$1/
+    chmod -R 770 /var/www/$1/*
+
+
+    echo "Usuario " + $1 + " creado"
+    echo "===============ANOTE================"
+    echo "||         LA CONTRASEÑA          ||"
+    echo "||                                ||"
+    echo "||                                ||"
+    echo "||  $password_generada ||"
+    echo "||                                ||"
+    echo "||                                ||"
+    echo "===================================="
+
+    chmod 755 /var/www/$1/
+    chown -R $1:$1 /var/www/$1/
+    chown root:root /var/www/$1/
+    chmod -R 770 /var/www/$1/*
+
+    read -n1 -p "¿Crear el sitio de Apache utilizando la configuración por defecto? [s/n]: " apache_defecto
+
+    case $apache_defecto in
+        n|N) crear_apache($1);;
+        *) read -p "URL para el sitio (se añadirá .iaw.com)" url_sitio
+           crear_apache_mod($1);;
+    asec
+}
+
+#Función BORRAR
+function borar(){
+    
+}
+
+#Función MODIFICAR
+function modificar (){
+    
+}
+
+
+select=$0
+
+while $select !=5; do
+    echo "1. Listar usuarias"
+    echo "2. Crear usuarios"
+    echo "3. Borrar usuarias"
+    echo "4. Modificar usuarios"
+done
+
 echo "Creando usuario" + $1
 
 read -p "Pulsa cualquier tecla para continuar"
@@ -54,12 +124,5 @@ printf "ChrootDirectory /var/www/$1" >> /etc/ssh/sshd_config.d/$1.conf
 #<usuario>.iaw.com
 #blog.<usuario>.iaw.com
 
-
-En sshd_config
-    Match User *
-    Include /etc/ssh/sshd_config.d/*
-
-En sshd_config.d/pepito.conf
-        ChrootDirectory %h
-        ForceCommand internal-sftp -u 0027
-        PasswordAuthentication yes
+# REFERENCIAS:
+# https://devhints.io/bash
